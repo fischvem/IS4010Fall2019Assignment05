608a46b1be6bf199ec8d22efca0a1b27d79066b1
efisc <efisc@DESKTOP-D7EDQ3C.wifi.uc.edu>
2019-09-27 10:01:48 -0400

    assignment complete

--- IS4010Fall2019Assignment05/src/footballFun/FootballFun.java
+++ IS4010Fall2019Assignment05/src/footballFun/FootballFun.java
@@ -1,8 +1,8 @@
 /*
- * IS4010 Fal 2019
+ * IS4010 Fall 2019
  * Assignment 05
- * Bill Nicholson
- * nicholdw@ucmail.uc.edu
+ * Emily Fischvogt
+ * fischvem@mail.uc.edu
  */
 package footballFun;
 
@@ -22,10 +22,34 @@ public class FootballFun {
 		if (correctCount == 8) {
 			System.out.println("All tests passed");
 		} else {
-			System.out.println("At least one test failed");
+			System.out.println("At least one test failed"); 
 		}
 	}
+	/**
+	 * 
+	 * Returning string that describes score number input
+	 * @param score
+	 * @return describes the score
+	 */
 	public static String translateScore (int score) {
+		
+		if (score == 2) //if the integer score equals 2, then the string describes the score as safety
+			return ("safety");
+		else if (score == 3) //if the integer score equals 3, then the string describes the score as field goal
+			return ("field goal");
+		else if (score == 6) //if the integer score equals 6, then the string describes the score as touchdown
+			return ("touchdown");
+		else if (score == 7) //if the integer score equals 7, then the string describes the score as touchdown and extra point
+			return ("touchdown and extra point");
+		else if (score == 8) //if the integer score equals 8, then the string describes the score as touchdown and 2-point conversion
+			return ("touchdown and 2-point conversion");
+		else if (score == -1) //if the integer score equals -1, then the string describes the score as a blank space
+			return ("");
+		else if (score == 1) //if the integer score equals 1, then the string describes the score as invalid
+			return ("invalid");
+		else if (score == 10) //if the integer score equals 10, then the string describes the score as you must be playing Quidditch
+			return ("you must be playing Quidditch ");
+		//Checking strings to see if they are exactly the same object
 		return "";
 	}
 }
